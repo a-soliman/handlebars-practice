b9287a5b93030c86ac1264dff64786014c2e4d58
Ahmed Soliman <ahmedsoliman@Ahmed-Solimans-MacBook-Pro.local>
2017-08-15 16:45:08 -0700

    changing to json and setup gulp

--- js/main.js
+++ js/main.js
@@ -1,43 +1,19 @@
-const cast = {
-	"characters": [
-		{
-			"name": "John Snow",
-			"shortCode": "jon-snow",
-			"actor": "Kit Harrington",
-			"house": "Stark",
-			"location": "The Wall"
-		},
-		{
-			"name": "Tyrion Lannister",
-			"shortCode": "tyrion",
-			"actor": "Peter Dinklarge",
-			"house": "Lannister"
-		},
-		{
-			"name": "Brienne Of Tarth",
-			"shortCode": "brienne",
-			"actor": "Gwendoline Christie",
-			"house": "Clegane",
-			"location": undefined
-		},
-		{
-			"name": "Eddard Stark",
-			"shortCode": "ned-stark",
-			"actor": "Sean Bean",
-			"house": "Stark",
-			"location": "winterfell"
-		},
-		{
-			"name": "Sandor Clegane",
-			"shortCode": "the-hound",
-			"actor": "Rory McCann",
-			"house": "Clegane",
-			"location": "unknown"
+$(document).ready(function() {
+	$.ajax({
+		type:'GET',
+		dataType: 'json',
+		async: false,
+		url: '../data/cast.json',
+		success: function(data) {
+			console.log(data);
 		}
-	]
-}
+	})
+})
 
 
 var charactorTemplate = $("#characterTemplate").html();
 var compiledCharactor = Handlebars.compile(charactorTemplate);
-$('.characters-container').html(compiledCharactor(cast))
+
+
+
+//$('.characters-container').html(compiledCharactor(cast))
