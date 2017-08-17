fb7863233b3055f9072aa8f7e7c0a7f65ffa7977
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-08-17 06:27:02 -0700

    practicing registerHelber method

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+Handlebars.registerHelper("formatName", function(property1, property2) {
+	return new Handlebars.SafeString(
+		"Hello my name is <strong>" + property1 + "</strong>, and I live at " + property2
+		);
+});
+Handlebars.registerHelper("formatPhoneNumber", function(property) {
+	var phone = property.toString();
+	return "(" + phone.substr(0,3) + ")" + phone.substr(3,3) + "-" + phone.substr(6,4);
+});
+
+Handlebars.registerHelper("makeBold", function(options) {
+	return new Handlebars.SafeString("<strong>" + options.fn(this) + "</strong>");
+});
+
+Handlebars.registerHelper("upperCase", function(options) {
+	if(options) {
+		return options.fn(this).toUpperCase();
+	}
+});
+
 $(document).ready(function() {
 	var charactorTemplate = $("#characterTemplate").html();
 var compiledCharactor = Handlebars.compile(charactorTemplate);
